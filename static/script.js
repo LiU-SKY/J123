f5de7d9f8b10845ea012ec42c4114075653e644e
j30n9hn <j30n9hn@gmail.com>
2025-04-07 15:24:56 +0900

    add static

---
+++ static/script.js
@@ -0,0 +1,18 @@
+async function fetchData() {
+    const response = await fetch('/data');
+    const data = await response.json();
+    const list = document.getElementById('data-list');
+    list.innerHTML = '';
+    data.forEach(item => {
+        const li = document.createElement('li');
+        li.textContent = item;
+        list.appendChild(li);
+    });
+}
+
+// 처음 로딩 시 실행
+fetchData();
+
+// 3초마다 자동 생신
+setInterval(fetchData, 3000);
+
