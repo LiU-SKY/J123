89195197190e63ede7f74ce23e2e32cc5ca4d2b9
j30n9hn <j30n9hn@gmail.com>
2025-05-01 00:11:39 +0900

    Update: change array to mongoDB

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask, request, jsonify, render_template, redirect, url_for
+from pymongo import MongoClient
+
 app = Flask(__name__)
 
+client = MongoClient('mongodb://localhost:27017/')
+db = client['DroneDB']  # 데이터베이스 이름
+collection = db['drones']  # 컬렉션 이름
+
 # 임시 저장소 (DB 대신 메모리 사용)
-data_storage = []
+data_storage = list(collection.find({}, {'_id': False}))  # _id 제외
 
 @app.route('/')
 def index():
@@ -18,10 +24,19 @@ def register():
    
 @app.route('/submit/', methods=['POST'])
 def submit():
-      new_data = request.form.get('value') # 예: HTML form에서 value 전송
-      if new_data:
-         data_storage.append(new_data)
-      return redirect(url_for('register'))
+   data = request.json
+   collection.insert_one(data)
+   return redirect(url_for('register'))
+
+'''new_data = request.form.get('value') # 예: HTML form에서 value 전송
+if new_data:
+   data_storage.append(new_data)
+return redirect(url_for('register'))'''
+
+'''@app.route('/all')
+def get_all():
+    all_data = list(collection.find({}, {'_id': False}))  # _id 제외
+    return jsonify(all_data)'''
 
 @app.route('/position/')
 def get_data():
