f25b3566b8bb5feda76b88b523703402b79fef67
j30n9hn <j30n9hn@gmail.com>
2025-06-01 00:26:22 +0900

    Add: Adding tag crud route, annotation drone route, connect to the database

--- app.py
+++ app.py
@@ -1,46 +1,38 @@
-from flask import Flask, request, jsonify, render_template, redirect, url_for
+from flask import Flask, request, flash, render_template, redirect, url_for
 import db
 
 app = Flask(__name__)
+app.secret_key = "your_secret_key" 
 
 @app.route('/')
 def index():
-
-   return render_template('index.html', data = db.get_all_tags())
+   result, statusCode = db.get_all_tags()
+   return render_template('index.html', data = result["tags"])
 
 @app.route('/logging/')
 def logging():
-   return render_template('logging.html', data = db.get_all_tags())
+   result, statusCode = db.get_all_tags()
+   return render_template('logging.html', data = result["tags"])
 
 @app.route('/register/')
 def register():
-   data=db.get_all_tags()
-   return render_template('register.html', data=data)
+   result, statusCode = db.get_all_tags()
+   return render_template('register.html', data = result["tags"])
 
+'''
  # 드론 등록
 @app.route('/submit/register/drone/', methods=['POST'])
 def registerDrone():
-   #data = request.json
-   #collection.insert_one(data)
-   droneName = request.form.get('droneName')
-   doc = {
-      "name": droneName,
-      "status": None,
-      "location": None,
-      "tag": {"mac_address": None,
-              "tag_name": None,
-              "location": None
-            }
-   }
-   collection.insert_one(doc)
    return redirect(url_for('register'))
-
+'''
+'''
 # 드론 삭제
 @app.route('/submit/delete/drone/', methods=['POST'])
 def deleteDrone():
    droneName = request.form.get('deleteDrone')
    collection.delete_one({"name": droneName})
    return redirect(url_for('register'))
+'''
 
 # 태그 등록
 @app.route('/submit/register/tag/', methods=['POST'])
@@ -48,12 +40,35 @@ def registerTag():
    macAddress = request.form.get('macAddress')
    tagName = request.form.get('tagName')
    location = request.form.get('location')
-   db.register_tag(macAddress, tagName, location)
+   result, statusCode = db.register_tag(macAddress, tagName, location)
+   if statusCode != 201:
+      flash(result["error"], "error")
+   else:
+      flash(result["message"], "success")
    return redirect(url_for('register'))
 
-@app.route('/position/')
-def get_data():
-    return jsonify(data_storage)
+# 태그 삭제
+@app.route('/submit/delete/tag/', methods=['POST'])
+def deleteTag():
+   macAddress = request.form.get('deleteTag')
+   result, statusCode = db.delete_tag(macAddress)
+   if statusCode != 200:
+      flash(result["error"], "error")
+   else:
+      flash(result["message"], "success")
+   return redirect(url_for('register'))
+
+# 태그 수정
+@app.route('/submit/edit/tag/', methods=['POST'])
+def editTag():
+   macAddress = request.form.get('macAddress')
+   tagName = request.form.get('tagName')
+   result, statusCode = db.update_tag(macAddress, tagName)
+   if statusCode != 200:
+      flash(result["error"], "error")
+   else:
+      flash(result["message"], "success")
+   return redirect(url_for('register'))
 
 if __name__ == '__main__':  
    app.run('0.0.0.0', port=5000, debug=True)
